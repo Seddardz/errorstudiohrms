4df09ae52a04ae846fe62de4d7697b7721dd8593
unknown <ephmaghni@gmail.com>
2023-07-12 21:38:59 +0200

    3b

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -27,23 +27,23 @@
 <form action="{{ url('register_post') }}" method="post">
 {{ csrf_field() }}
         <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Name" name="name">
+        <input type="text" class="form-control" placeholder="Name" name="name" required value="{{ old('name') }}">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
             </div>
           </div>
         </div>
-
+<span style="color:red;">{{ $errors->first('name') }}</span>
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+        <input type="email" class="form-control" placeholder="Email" name="email" required value="{{ old('email') }}">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
             </div>
           </div>
         </div>
-
+<span style="color:red;">{{ $errors->first('email') }}</span>
         <div class="input-group mb-3">
         <input type="password" class="form-control" placeholder="Password" name="password">
           <div class="input-group-append">
@@ -52,7 +52,7 @@
             </div>
           </div>
         </div>
-
+<span style="color:red;">{{ $errors->first('password') }}</span>
         <div class="input-group mb-3">
           <input type="password" class="form-control" placeholder="Confirm Password" name="confirm_password">
           <div class="input-group-append">
@@ -60,7 +60,8 @@
               <span class="fas fa-lock"></span>
             </div>
           </div>
-        </div>        
+        </div>
+<span style="color:red;">{{ $errors->first('confirm_password') }}</span>        
         <div class="row">
           <div class="col-8">
             <div class="icheck-primary">
