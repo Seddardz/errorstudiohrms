135004d60c56fe48fde8058b9dae294bfc7ef8d3
unknown <ephmaghni@gmail.com>
2023-07-17 11:08:15 +0200

    employee list blade create

--- resources/views/backend/employees/list.blade.php
+++ resources/views/backend/employees/list.blade.php
@@ -1,59 +1,89 @@
 @extends('backend.layouts.app')
 @section('content')
-
-<!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Employees</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">List</a></li>
-              <li class="breadcrumb-item active">Employees</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-
-
-    <section class="content">
-    <div class="container-fluid">
-@include('_message')
-<a href="{{url('admin/employees/add')}}" class="btn btn-primary">Add Employees</a>
-        <div class="row">
-            <section class="col-lg-12">
-                <div class="card">
-                    <div class="card-header">
-                        <table class="table table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>First name</th>
-                                    <th>Last name</th>
-                                    <th>Email</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>seddar</td>
-                                    <td>mohammed</td>
-                                    <td>seddar@gmail.com</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </section>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Employees</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6" style="text-align: right;">
+                        <a href="{{ url('admin/employees/add') }}" class="btn btn-primary">Add Employees</a>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
         </div>
-    </div>
-</section>
-    </div>
-  <!-- /.content-wrapper -->
 
+        <section class="content">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <section class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="search-title">Search Employee</h3>
+                            </div>
+                            <form method="GET" action="">
+                                <div class="card-body">
+                                    <div class="row">
+
+                                        <div class="form-group col-md-3">
+                                            <label>First Name</label>
+                                            <input type="text" name="" class="form-control"
+                                                placeholder="First name">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <label>Last Name</label>
+                                            <input type="text" name="" class="form-control"
+                                                placeholder="Last name">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <button class="btn btn-primary" type="submit"
+                                                style="margin-top:30px">Search</button>
+                                            <a href="{{ url('admin/employees') }}" class="btn btn-success"
+                                                style="margin-top: 30px">Reset</a>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </form>
+                        </div>
+                        @include('_message')
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Employee List</h3>
+                                <div class="card-body p-0">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>*</th>
+                                                <th>First name</th>
+                                                <th>Last name</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>sed</td>
+                                                <td>moh</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <div style="padding:10px; float:right;">
+                                        Pagination
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </section>
+    </div>
+    <!-- /.content-wrapper -->
 @endsection
